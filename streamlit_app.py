ea10a77e2af42a5fda5b8695111f279136caba2b
agungmagnetcuan-ai <agungmagnetcuan@gmail.com>
2026-05-22 22:05:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,12 @@ st.title("🎈 My new appp agung")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+number = st.number_input(
+    "Insert a number", value=None, placeholder="Type a number..."
+)
+st.write("The current number is ", number)
+
+
+
