22c528c2064af512387bff90d026a80c2c458f26
agungmagnetcuan-ai <agungmagnetcuan@gmail.com>
2026-05-22 22:09:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.write(
 
 
 number = st.number_input(
-    "masukan nomor panjang" value=None, placeholder="Type a number..."
+    "masukan nomor panjang", value=None, placeholder="Type a number..."
 )
 st.write("The current number is ", number)
 
